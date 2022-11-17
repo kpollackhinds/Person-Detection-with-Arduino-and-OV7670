50b8b906354b964ed2d67bc6428717e5793363a4
Kahlil Pollack-Hinds <84944416+kpollackhinds@users.noreply.github.com>
2022-11-17 14:57:18 -0500

    Updated file directory

---
+++ Object Detection/sketch_nov17a/sketch_nov17a.ino
@@ -0,0 +1,13 @@
+#include <Arduino_OV767X.h>
+//#include <TinyMLShield.h>
+#include <TensorFlowLite.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
